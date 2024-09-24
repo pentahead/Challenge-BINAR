1c1f0cca4798a86e5df3183b0381bf1b3b503576
pentahead <dhiyaafaruq@gmail.com>
2024-09-24 20:26:48 +0700

    add function practice

--- Pertemuan/P4-DOM/scripts/index.js
+++ Pertemuan/P4-DOM/scripts/index.js
@@ -1,80 +1,64 @@
-// import json data from data/student.json
-import studentData from "../data/student.json" with {type:"json"};
+  // import json data from data/student.json
+  import studentData from "../data/student.json" with {type:"json"};
 
-// with common js 
-// const studentsData = require("../data/students.json")
-console.log(studentData);
+  // with common js 
+  // const studentsData = require("../data/students.json")
+  console.log(studentData);
 
-const studentContent = document.getElementById("student-content");
+  const studentContent = document.getElementById("student-content");
+  searchStudentContent("")
 
-// helper variable
-let studentContentHTML = ""; 
+  const search = document.getElementById("search");
+  const searchForm = document.getElementById("search-form");
 
+  search.addEventListener("input", (e) => {
+    const searchValue = e.target.value.toLowerCase();
+    searchStudentContent(searchValue)
+  });
 
-function renderStudents(data) {
-    let htmlContent = "";
-    data.map((student) => { 
-        
-        const studentContent = `
-            <div class="col-md-3 m-3">
-              <div class="card" style="width: 18rem">
-                <div class="card-body">
-                  <h5 class="card-title">${student.name}</h5>
-                  <h6 class="card-subtitle mb-2 text-body-secondary">
-                   ${student.education.bachelor}
-                  </h6>
-                  <p class="card-text">
-                    My name is ${student.name}, used to be called ${student.nickname}, I am from ${student.address.city}, ${student.address.province}. And I am a student of ${student.education.bachelor}.
-                  </p>
-                  <a href="#" class="card-link">Card link</a>
-                  <a href="#" class="card-link">Another link</a>
+  search.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  function searchStudentContent(search) {
+  studentContent.innerHTML = "<h1> Loading...</h1>";
+
+  const filteredStudents = studentData.filter((student) => {
+    return student.name.toLowerCase().includes(search) || student.education.bachelor.toLowerCase().includes(search);
+
+  });
+
+  let studentContentHTML ="";
+  filteredStudents.map((student) => {
+    const studentContent = `
+              <div class="col-md-3 m-3">
+                <div class="card" style="width: 18rem">
+                  <div class="card-body">
+                    <h5 class="card-title">${student.name}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">
+                    ${student.education.bachelor}
+                    </h6>
+                    <p class="card-text">
+                      My name is ${student.name}, used to be called ${student.nickname}, I am from ${student.address.city}, ${student.address.province}. And I am a student of ${student.education.bachelor}.
+                    </p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                  </div>
                 </div>
               </div>
-            </div>
-        `;
-        htmlContent += studentContent;
-    });
-    return htmlContent;
-}
+          `;
+          studentContentHTML += studentContent;
+  });
+  studentContent.innerHTML = studentContentHTML;
+    
+  }
 
-// Initial loading message
-studentContent.innerHTML = "<h1> Loading...</h1>";
+  const getStudentData = async () => {
+    const response = await fetch("/data/student.json");
+    const  data = await response.json();
+    return data;
+  };
 
-// Render all students after timeout
-setTimeout(() => {
-    studentContentHTML = renderStudents(studentData); 
-    studentContent.innerHTML = studentContentHTML;
-}, 3000);
-
-// Start search functionality
-const search = document.getElementById("search");
-
-
-search.addEventListener("input", (e) => {
-    const searchValue = e.target.value.toLowerCase();
-    const filteredStudents = studentData.filter(student => 
-        student.name.toLowerCase().includes(searchValue) || 
-        student.nickname.toLowerCase().includes(searchValue) || 
-        student.education.bachelor.toLowerCase().includes(searchValue)
-    );    
-    studentContent.innerHTML = renderStudents(filteredStudents);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-//-------------------------------------------------------
-// test dom title
-// const title = document.getElementById("title");
-// setTimeout(() => {
-//   console.log("I Executed after 5 seconds program run!");
-//   title.innerText = "Document Object Mapping";
-// }, 5000);
+  getStudentData().then(() => {
+    searchStudentContent("");
+  });
