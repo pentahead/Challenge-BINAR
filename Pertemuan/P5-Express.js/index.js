75540ceccd1116b2afd80c8d30a18b21c3aef7a5
pentahead <dhiyaafaruq@gmail.com>
2024-09-30 22:09:36 +0700

    add: latihan routing expressJS

--- Pertemuan/P5-Express.js/index.js
+++ Pertemuan/P5-Express.js/index.js
@@ -1,15 +1,37 @@
-const express = require('express'); // import with non module
+const express = require("express"); // import with non module
+const students = require("./data/student.json"); // import data students
 
 //make/Initialize express application
-const app = express()
-const port = 4000
+const app = express();
+const port = 3000;
 
 // Make a routing and responses
-app.get('/', (req, res) => {
-  res.send('Hello World!, im using nodemon')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!, im using nodemon");
+});
+
+//membuat routing baru
+app.get("/students", (req, res) => {
+  res.json(students); //meminta response file json
+});
+
+app.get("/students/:id", (req, res) => {
+  const { id } = req.params; //get the id form params
+
+  //find stuudent by id
+  const student = students.find((student) => student.id == id);
+  console.log(student);
+  // if student has been found, it will be response the student data
+  if (student) {
+    res.json(student);
+    return;
+  }
+
+  // if there is no student with the id that client request , it will response not found
+  res.status(404).json({ message: "student not found! nih" });
+});
 
 // Run the express.js application
 app.listen(port, () => {
-  console.log(`The express.js is running on port ${port}`)
-})
+  console.log(`The express.js is running on port ${port}`);
+});
