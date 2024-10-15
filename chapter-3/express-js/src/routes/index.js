189e037892bc9a0b9859a3ed352a2a57c52fc266
pentahead <dhiyaafaruq@gmail.com>
2024-10-15 22:47:29 +0700

    delete unused files

--- chapter-3/express-js/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const studentsRouter = require("./students");
-
-const router = express.Router();
-
-router.use("/students", studentsRouter);
-
-module.exports = router;
